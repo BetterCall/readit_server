880b93ebcfdf8d5b4bd8d677cf24a5ebbd01e706
Bryann <bryann@brovia.com>
2020-03-13 07:26:50 +0100

    update post model

--- src/entities/Post.js
+++ src/entities/Post.js
@@ -15,7 +15,7 @@ export class Post extends BaseEntity {
   @Column({ type: "text" })
   postUrl;
 
-  @Column({ type: "decimal", default: 0, scale: 2 })
+  @Column({ type: "decimal", default: 0.4, scale: 2 })
   price;
 
   @CreateDateColumn()
