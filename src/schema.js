dba22547e5c2300952eeda926cc25e5ce324d24a
Bryann <bryann@brovia.com>
2020-03-13 07:12:06 +0100

    path import

--- src/schema.js
+++ src/schema.js
@@ -1,4 +1,4 @@
-import path from "path";
+import * as path from "path";
 
 import { makeExecutableSchema } from "graphql-tools";
 import { fileLoader, mergeResolvers, mergeTypes } from "merge-graphql-schemas";
