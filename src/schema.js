15f2ba826338dd00e63be601bd820f78276ef9ef
Bryann <bryann@brovia.com>
2020-03-13 06:58:16 +0100

    gtzpqhl model

--- src/schema.js
+++ src/schema.js
@@ -3,7 +3,7 @@ import path from "path";
 import { makeExecutableSchema } from "graphql-tools";
 import { fileLoader, mergeResolvers, mergeTypes } from "merge-graphql-schemas";
 
-const allTypes = fileLoader(path.join(__dirname, "/api/**/**/*.graphql"));
+const allTypes = fileLoader(path.join(__dirname, "/api/**/*.graphql"));
 const allResolvers = fileLoader(path.join(__dirname, "/api/**/*.js"));
 
 const schema = makeExecutableSchema({
