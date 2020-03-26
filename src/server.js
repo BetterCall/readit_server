1ba7d1e7fe6be8a50103414410e153227b5b9e2b
Bryann <bryann@brovia.com>
2020-03-26 20:04:06 +0100

    try catch

--- src/server.js
+++ src/server.js
@@ -34,35 +34,39 @@ server.express.post("/api/post", async (req, res, next) => {
   if (!postUrl || !price) {
     res.send("error");
   }
-  //await Post.create({ postUrl, price, ip }).save();
-  const attempt = await Attempt.findOne({ ip });
-  if (attempt) {
-    attempt.count = attempt.count + 1;
-    attempt.save();
-  } else {
-    const results = await getRepository(Post)
-      .createQueryBuilder("post")
-      .select("ip")
-      .addSelect("COUNT(*) AS count")
-      .groupBy("post.ip")
-      .getRawMany();
+  try {
+    //await Post.create({ postUrl, price, ip }).save();
+    const attempt = await Attempt.findOne({ ip });
+    if (attempt) {
+      attempt.count = attempt.count + 1;
+      attempt.save();
+    } else {
+      const results = await getRepository(Post)
+        .createQueryBuilder("post")
+        .select("ip")
+        .addSelect("COUNT(*) AS count")
+        .groupBy("post.ip")
+        .getRawMany();
 
-    const found = false;
-    Promise.all(
-      results.map(async result => {
-        if (result.ip == ip) {
-          await Attempt.create({ ip, count: parseInt(result.count) }).save();
-          found = true;
-        }
-      })
-    );
+      const found = false;
+      Promise.all(
+        results.map(async result => {
+          if (result.ip == ip) {
+            await Attempt.create({ ip, count: parseInt(result.count) }).save();
+            found = true;
+          }
+        })
+      );
 
-    if (!found) {
-      await Attempt.create({
-        ip: result.ip,
-        count: 1
-      }).save();
+      if (!found) {
+        await Attempt.create({
+          ip: result.ip,
+          count: 1
+        }).save();
+      }
     }
+  } catch ({ message }) {
+    console.log(message);
   }
 
   res.send("merci :)");
